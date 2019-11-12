26c11533f0dfb17d19b2fc5f5d19ecff4a8deca2
Clayton Jones <claytonjonesLMP@gmail.com>
2019-11-12 15:44:05 -0800

    removed unnecessary comments and other chunks of code

--- app.js
+++ app.js
@@ -1,61 +1,14 @@
 'use-strict';
 
-// store element location
-// var firstAndPikeElement = document.getElementById('firstAndPike');
-// console.log(firstAndPikeElement);
-// create element
-// var liEl = document.createElement('li');
-// // give element content
-// liEl.textContent = 'proof of life';
-// // append to the DOM
-// firstAndPikeElement.appendChild(liEl);
-
+// global array of hours shared by all locations
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
-// var firstAndPikeShop = {
-//   name: 'First and Pike',
-//   minCostomersEachHour: 23,
-//   maxCustomersEachHour: 65,
-//   averageCookiesPerCustomer: 6.3,
-//   customersEachHour: [],
-//   cookiesEachHour: [],
-//   totalCookiesForTheDay: 0,
-
-//   randomNumber: function(min, max) {
-//     return Math.floor(Math.random() * (max + 1 - min)) + min;
-//   },
-
-//   render: function() {
-//     for ( var i = 0; i < hours.length; i++ ) {
-//       // create element
-//       var liEl = document.createElement('li');
-//       // calculate number of customers for the hour, then add to array
-//       var customers = this.randomNumber(this.minCostomersEachHour, this.maxCustomersEachHour);
-//       this.customersEachHour.push(customers);
-//       // calculate number of cookies for the hour, then add to array
-//       var cookies = Math.round(customers * this.averageCookiesPerCustomer);
-//       this.cookiesEachHour.push(cookies);
-//       // give element content
-//       liEl.textContent = `${hours[i]}: ${cookies} cookies`;
-//       // append to DOM
-//       firstAndPikeElement.appendChild(liEl);
-//       this.totalCookiesForTheDay += cookies;
-//     }
-//     var liEl = document.createElement('li');
-//     liEl.textContent = `Total: ${this.totalCookiesForTheDay}`
-//     firstAndPikeElement.appendChild(liEl);
-//   }
-// };
-
-// firstAndPikeShop.render();
 
 // global random number function to be used by objects
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max + 1 - min)) + min;
 }
 
-// store one store element that each object can use
-// store seattle element location
+// stores one store element that each object can use to append to
 var storeElement = document.getElementById('stores');
 
 var seattle = {
