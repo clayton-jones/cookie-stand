8a79f40a0e9aa13148c9cf1f7b385e73a5bcbe54
Clayton Jones <claytonjonesLMP@gmail.com>
2019-11-12 15:40:18 -0800

    Lima object working

--- app.js
+++ app.js
@@ -248,10 +248,9 @@ var paris = {
 };
 paris.render();
 
-// don't need, use single global from above
-var limaElement = document.getElementById('lima');
 
 var lima = {
+  name: 'Lima',
   minCostomersEachHour: 2,
   maxCustomersEachHour: 16,
   averageCookiesPerCustomer: 4.7,
@@ -259,42 +258,40 @@ var lima = {
   cookiesEachHour: [],
   totalCookiesForTheDay: 0,
 
-  randomNumber: function(min, max) {
-    return Math.floor(Math.random() * (max + 1 - min)) + min;
-  },
-
+  // loops through hours array and calculates random customers based on store min and max
   randomCustomers: function() {
-    var customers = this.randomNumber(this.minCostomersEachHour, this.maxCustomersEachHour);
-    this.customersEachHour.push(customers);
-    return customers;
-  },
-
-  calculateCookies: function(customers) {
-    var cookies = Math.round(customers * this.averageCookiesPerCustomer);
-    this.cookiesEachHour.push(cookies);
-    this.totalCookiesForTheDay += cookies;
-    return cookies;
-  },
-
-  updateLi: function(i, cookies) {
-    var liEl = document.createElement('li');
-    liEl.textContent = `${hours[i]}: ${cookies} cookies`;
-    limaElement.appendChild(liEl);
-  },
-
-  totalLi: function() {
-    var liEl = document.createElement('li');
-    liEl.textContent = `Total: ${this.totalCookiesForTheDay}`;
-    limaElement.appendChild(liEl);
-  },
-
-  render: function() {
-    for ( var i = 0; i < hours.length; i++ ) {
-      var customers = this.randomCustomers();
-      var cookies = this.calculateCookies(customers);
-      this.updateLi(i, cookies);
+    for (var i = 0; i < hours.length; i++) {
+      var customers = randomNumber(this.minCostomersEachHour, this.maxCustomersEachHour);
+      this.customersEachHour.push(customers);
     }
-    this.totalLi();
+  },
+
+  // loops through customerEachHour array and calculates cookies, populates cookiesEachHour array
+  calculateCookies: function() {
+    for (var i = 0; i < this.customersEachHour.length; i++) {
+      var cookies = Math.ceil(this.customersEachHour[i] * this.averageCookiesPerCustomer);
+      this.cookiesEachHour.push(cookies);
+      this.totalCookiesForTheDay += cookies;
+    }
+  },
+
+  // use arrays from above to get customers and cookies
+  render: function() {
+    this.randomCustomers();
+    this.calculateCookies();
+    var h2El = document.createElement('h2');
+    h2El.textContent = this.name;
+    storeElement.appendChild(h2El);
+    var ulEl = document.createElement('ul');
+    for ( var i = 0; i < hours.length; i++ ) {
+      var liEl = document.createElement('li');
+      liEl.textContent = `${hours[i]}: ${this.cookiesEachHour[i]} cookies`;
+      ulEl.appendChild(liEl);
+    }
+    liEl = document.createElement('li');
+    liEl.textContent = `Total: ${this.totalCookiesForTheDay}`;
+    ulEl.appendChild(liEl);
+    storeElement.appendChild(ulEl);
   }
 };
 lima.render();
